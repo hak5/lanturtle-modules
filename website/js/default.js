f8baf74a9adddfd22942b01630def675187375dd
Sebastian Kinne <contact@sebkinne.com>
2015-07-27 18:01:06 +0300

    Website, Wiki, Module repository going public

---
+++ website/js/default.js
@@ -0,0 +1,16 @@
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// Start carousel
+$(document).ready(function() {
+    $('#carousel').carousel({
+      interval: 7000
+    });
+});
---
+++ wiki/navigation.md
@@ -0,0 +1,9 @@
+# Hak5 Lan Turtle Wiki
+
+[Home](index.md)
+
+[Video Tutorials](videos.md)
+
+[Purchase](http://www.hakshop.com/lanturtle)
+
+[Forums](https://forums.hak5.org/index.php?/forum/88-lan-turtle/)
